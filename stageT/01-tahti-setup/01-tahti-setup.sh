2e51371ff841a4b24307d3d3b8d7a5f27745a7d9
Joachim Janz <joxda@yahoo.de>
2025-03-08 18:36:19 +0200

    Update 01-tahti-setup.sh

--- stageT/01-tahti-setup/01-tahti-setup.sh
+++ stageT/01-tahti-setup/01-tahti-setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+echo "Start"
 sudo -u tahti mkdir /usr/local/tahti
 cd /usr/local/tahti
+pwd
 git clone https://github.com/joxda/astro-soft-build.git
 cd astro-soft-build
+ls
 bash build-fxload.sh
 bash build-soft-stable.sh phd2
 bash build-more.sh
 bash installPythonRelated.sh
 bash extra-setup.sh
+echo "End"
