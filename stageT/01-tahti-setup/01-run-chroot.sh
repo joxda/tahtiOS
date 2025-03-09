5a1e3a4bee2c97ca2b1d2c7690ef67bb917c7937
Joachim Janz <joxda@yahoo.de>
2025-03-10 01:51:54 +0200

    Update 01-run-chroot.sh

--- stageT/01-tahti-setup/01-run-chroot.sh
+++ stageT/01-tahti-setup/01-run-chroot.sh
@@ -14,7 +14,7 @@ cd /usr/local/tahti
 git clone https://github.com/joxda/astro-soft-build.git
 cd astro-soft-build
 ls
-bash build-fxload.sh
+#bash build-fxload.sh
 bash build-soft-stable.sh phd2
 bash build-more.sh
 bash installPythonRelated.sh
