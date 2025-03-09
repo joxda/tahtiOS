315884e66491317bbf65593a7ec9acc920077db2
Joachim Janz <joxda@yahoo.de>
2025-03-09 16:11:48 +0200

    Update 01-run-chroot.sh

--- stageT/01-tahti-setup/01-run-chroot.sh
+++ stageT/01-tahti-setup/01-run-chroot.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 mkdir /usr/local/tahti
 cd /usr/local/tahti
-pwd
+echo 'export CFLAGS="-march=armv8-a -mtune=cortex-a72 -mno-outline-atomics"' >> /etc/profile
+echo 'export CXXFLAGS="-march=armv8-a -mtune=cortex-a72 -mno-outline-atomics"' >> /etc/profile
+echo 'export CC="aarch64-linux-gnu-gcc"' >> /etc/profile
+echo 'export CXX="aarch64-linux-gnu-g++"' >> /etc/profile
+export CFLAGS="-march=armv8-a -mtune=cortex-a72 -mno-outline-atomics"
+export CXXFLAGS="-march=armv8-a -mtune=cortex-a72 -mno-outline-atomics"
+export CC="aarch64-linux-gnu-gcc"
+export CXX="aarch64-linux-gnu-g++"
+
 git clone https://github.com/joxda/astro-soft-build.git
 cd astro-soft-build
 ls
