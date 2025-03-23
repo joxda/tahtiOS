5e06e016bdde45ab0f8c33a600b564048cca79a8
joxda <joxda@yahoo.de>
2025-03-23 10:13:22 +0200

    d

--- stageT/01-tahti-setup/01-run.sh
+++ stageT/01-tahti-setup/01-run.sh
@@ -2,19 +2,22 @@
 set -ex
 echo "ROOTFS_DIR is set to: $ROOTFS_DIR"
 install -v -d "${ROOTFS_DIR}/usr/local/tahti"
+install -m 644 files/*zip ${ROOTFS_DIR}/usr/local/tahti
 ls $ROOTFS_DIR/usr/local/
 on_chroot <<EOF
 install -v -d "${ROOTFS_DIR}/usr/local/tahti"
 cd /usr/local/tahti
+unzip *zip
+tar xzvf -C / *tar.gz
 git clone https://github.com/joxda/astro-soft-build.git
 cd astro-soft-build
-echo "cmake_minimum_required(VERSION 3.0)" > CMakeLists.txt
-echo "project(TempProject)" >> CMakeLists.txt
-echo "find_package(USB1 REQUIRED)" >> CMakeLists.txt
-cmake . -LA | grep USB1
+#echo "cmake_minimum_required(VERSION 3.0)" > CMakeLists.txt
+#echo "project(TempProject)" >> CMakeLists.txt
+#echo "find_package(USB1 REQUIRED)" >> CMakeLists.txt
+#cmake . -LA | grep USB1
 chmod +x *.sh
 #bash build-fxload.sh
-./build-soft.sh #phd2
+#./build-soft.sh #phd2
 ./build-more.sh
 ./installPythonRelated.sh
 ./extra-setup.sh
