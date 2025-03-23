9670352a3f79cd102604bde9b576065831ebd3bf
joxda <joxda@yahoo.de>
2025-03-23 12:05:25 +0200

    y

--- stageT/01-tahti-setup/01-run.sh
+++ stageT/01-tahti-setup/01-run.sh
@@ -2,7 +2,7 @@
 set -ex
 echo "ROOTFS_DIR is set to: $ROOTFS_DIR"
 install -v -d "${ROOTFS_DIR}/usr/local/tahti"
-install -m 644 files/*zip ${ROOTFS_DIR}/usr/local/tahti
+install -m 644 files/*zip "${ROOTFS_DIR}/usr/local/tahti/"
 ls $ROOTFS_DIR/usr/local/
 on_chroot <<EOF
 install -v -d "${ROOTFS_DIR}/usr/local/tahti"
