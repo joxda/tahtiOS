aa63ae061dcc7534b170cd93a2d4b93576dc9f15
joxda <joxda@yahoo.de>
2025-04-11 06:52:40 +0300

    d

--- stageT/01-tahti-setup/01-run.sh
+++ stageT/01-tahti-setup/01-run.sh
@@ -4,10 +4,10 @@ echo "ROOTFS_DIR is set to: $ROOTFS_DIR"
 install -v -d "${ROOTFS_DIR}/usr/local/tahti"
 install -m 644 files/*tar.gz "${ROOTFS_DIR}/usr/local/tahti/"
 install -m 644 files/*deb "${ROOTFS_DIR}/usr/local/tahti/"
-install -m 644 -D files/autologin.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
-install -m 644 -D files/rc.xml "${ROOTFS_DIR}/home/tahti/.config/labwc/"
-install -m 644 -D files/themerc-override "${ROOTFS_DIR}/home/tahti/.config/labwc/"
-install -m 644 -D files/desktop-items-NOOP-1.conf "${ROOTFS_DIR}/home/tahti/.config/pcmanfm/LXDE-pi/"
+install -m 644 -D -o tahti files/autologin.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+install -m 644 -D -o tahti files/rc.xml "${ROOTFS_DIR}/home/tahti/.config/labwc/rc.xml"
+install -m 644 -D -o tahti files/themerc-override "${ROOTFS_DIR}/home/tahti/.config/labwc/themerc-override"
+install -m 644 -D -o tahti files/desktop-items-NOOP-1.conf "${ROOTFS_DIR}/home/tahti/.config/pcmanfm/LXDE-pi/desktop-items-NOOP-1.conf"
 on_chroot <<EOF
 cd /usr/local/tahti
 echo *.tar.gz
@@ -82,6 +82,7 @@ systemctl enable wayvnc.service
 rm -f /etc/nginx/sites-enabled/default
 rm -rf /var/log/*
 install -v -d "/var/log/nginx"
+chown -R tahti:tahti /usr/local/tahti
 rm -rf /tmp/*
 rm -rf /var/tmp/*
 apt-get -y autoremove && apt-get -y clean
