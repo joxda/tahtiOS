ceec44a83b28d814b5d0d74ce1c45123e8b87a36
Joachim Janz <joxda@yahoo.de>
2025-04-12 07:52:35 +0300

    Update 01-run.sh

--- stageT/01-tahti-setup/01-run.sh
+++ stageT/01-tahti-setup/01-run.sh
@@ -92,4 +92,9 @@ rm /usr/local/tahti/*deb
 sudo -u tahti dbus-launch dconf write /org/gnome/desktop/interface/gtk-theme "'PiXnoir'"
 sudo -u tahti dbus-launch dconf write /org/gnome/desktop/interface/font-name "'Piboto Condensed Regular'"
 EOF
-#rm files/*
+rm files/*
+sleep 15
+fuser -k "${ROOTFS_DIR}" || true
+lsof +D "${ROOTFS_DIR}" || true
+
+
